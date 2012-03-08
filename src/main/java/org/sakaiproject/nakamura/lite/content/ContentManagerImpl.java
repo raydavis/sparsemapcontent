62e1686db421e95e6fde4ac40a043dc09adbcbfb
Zach A. Thomas <zach@aeroplanesoftware.com>
2012-03-07 19:41:12 -0600

    KERN-2603 use entrySet() instead of keySet()

--- src/main/java/org/sakaiproject/nakamura/lite/content/ContentManagerImpl.java
+++ src/main/java/org/sakaiproject/nakamura/lite/content/ContentManagerImpl.java
@@ -818,8 +818,8 @@ public class ContentManagerImpl extends CachingManagerImpl implements ContentMan
         saveVersion.put(VERSION_NUMBER_FIELD, versionNumber);
       
         if (versionMetadata != null) {
-          for (String key : versionMetadata.keySet()) {
-            saveVersion.put("metadata:" + key, versionMetadata.get(key));
+          for (Entry<String, Object> entry : versionMetadata.entrySet()) {
+            saveVersion.put("metadata:" + entry.getKey(), entry.getValue());
           }
         }
 
